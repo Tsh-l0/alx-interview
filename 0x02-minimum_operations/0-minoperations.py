1ab55645a4e4e16364a58c7c7e8e49d7d1ed7e51
Pule Molefe <pmolefe25@gmail.com>
2025-05-03 02:47:17 +0000

    README.md

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function to calculate the minimum number of operations needed to achieve n H characters
+"""
+
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+        return operations
